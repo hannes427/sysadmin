3c64eed241c08fb2a9c9db52316a4ea04336a783
hannes427 <hannes.rusch@web.de>
2025-01-08 07:40:26 +0100

    removed test files

--- check_ip.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "functions.hpp"
-
-int main (int argc, char **argv) {
-    if (!validate_ip_address(argv[1], ","))
-        std::cout<<"Falsch"<<std::endl;
-    else
-        std::cout<<"Korrekt"<<std::endl;
-}
